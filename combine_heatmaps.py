99c184e83301be84579819f367e42be1cdca0455
cbaakman <c.baakman@umcn.nl>
2023-06-02 02:38:00 +0200

    some fixes

--- combine_heatmaps.py
+++ combine_heatmaps.py
@@ -6,11 +6,17 @@ import csv
 from glob import glob
 from tempfile import mkdtemp
 from shutil import rmtree
+from argparse import ArgumentParser
 
 import numpy
 import pandas
 from matplotlib import pyplot
 from moviepy.editor import ImageSequenceClip
+
+
+arg_parser = ArgumentParser(description="combine tcrspec csv tables into a heatmap animation")
+arg_parser.add_argument("complex_id", help="identifier for the complex, used in the csv filenames")
+arg_parser.add_argument("--head-id", "-H", help="identifier of the head, usually an integer", default="0")
 
 
 def get_epoch_number(path: str) -> float:
@@ -29,16 +35,16 @@ def get_epoch_number(path: str) -> float:
 
 if __name__ == "__main__":
 
-    complex_id = sys.argv[1]
-    head_id = sys.argv[2]
+    args = arg_parser.parse_args()
 
-    table_paths = glob(f"{complex_id}-*.*_h{head_id}.csv")
+    table_paths = glob(f"{args.complex_id}-*.*_h{args.head_id}.csv")
     table_paths = sorted(table_paths, key=get_epoch_number)
 
     storage_directory_path = mkdtemp()
     png_files = []
 
     for table_path in table_paths:
+        epoch_number = get_epoch_number(table_path)
 
         data = pandas.read_csv(table_path, sep=',', index_col=0, header=0)
 
@@ -46,7 +52,7 @@ if __name__ == "__main__":
         plot = figure.add_subplot()
         heatmap = plot.imshow(data, cmap="Greys", aspect="auto", vmin=0.0, vmax=0.3)
         figure.colorbar(heatmap)
-        pyplot.title(filename)
+        pyplot.title(f"{args.complex_id}, head:{args.head_id}, epoch:{epoch_number:.3f}")
 
         png_path = table_path.replace(".csv", ".png")
 
@@ -54,4 +60,4 @@ if __name__ == "__main__":
         png_files.append(png_path)
 
     clip = ImageSequenceClip(png_files, fps=10)
-    clip.write_videofile(f"{complex_id}_{head_id}.mp4", fps=15)
+    clip.write_videofile(f"{args.complex_id}_{args.head_id}.mp4", fps=15)
