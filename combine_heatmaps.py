6103f48b5c8cb891452f8ced215c01d904e58bd5
cbaakman <c.baakman@umcn.nl>
2023-06-02 02:30:05 +0200

    update this script to work with the new heatmap tables

--- combine_heatmaps.py
+++ combine_heatmaps.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import csv
+from glob import glob
 from tempfile import mkdtemp
 from shutil import rmtree
 
@@ -12,45 +13,45 @@ from matplotlib import pyplot
 from moviepy.editor import ImageSequenceClip
 
 
-def chronological(path: str) -> float:
+def get_epoch_number(path: str) -> float:
 
-    # train-epoch-11-batch-80-target-NONBINDING-crossweights-LLLENKSLT.csv
+    # BA-55224-20.160_h0.csv
     filename = os.path.basename(path)
 
-    words = filename.split('-')
+    word = filename.split('-')[-1].split('_h')[0]
+    numbers = word.split('.')
 
-    epoch_number = int(words[2])
-    batch_number = int(words[4])
+    epoch_number = int(numbers[0])
+    batch_number = int(numbers[1])
 
     return float(epoch_number) + 0.001 * batch_number
 
 
 if __name__ == "__main__":
 
-    table_directory_path = sys.argv[1]
+    complex_id = sys.argv[1]
+    head_id = sys.argv[2]
+
+    table_paths = glob(f"{complex_id}-*.*_h{head_id}.csv")
+    table_paths = sorted(table_paths, key=get_epoch_number)
 
     storage_directory_path = mkdtemp()
     png_files = []
-    try:
-        for filename in os.listdir(table_directory_path):
-            if "crossweights" in filename and filename.endswith(".csv"):
 
-                table_path = os.path.join(table_directory_path, filename)
+    for table_path in table_paths:
 
-                data = pandas.read_csv(table_path, sep=',', index_col=0, header=0)
+        data = pandas.read_csv(table_path, sep=',', index_col=0, header=0)
 
-                figure = pyplot.figure()
-                plot = figure.add_subplot()
-                heatmap = plot.imshow(data, cmap="Greys", aspect="auto", vmin=0.0, vmax=0.3)
-                figure.colorbar(heatmap)
-                pyplot.title(filename)
+        figure = pyplot.figure()
+        plot = figure.add_subplot()
+        heatmap = plot.imshow(data, cmap="Greys", aspect="auto", vmin=0.0, vmax=0.3)
+        figure.colorbar(heatmap)
+        pyplot.title(filename)
 
-                png_path = os.path.join(storage_directory_path, filename.replace(".csv", ".png"))
+        png_path = table_path.replace(".csv", ".png")
 
-                figure.savefig(png_path, format="png")
-                png_files.append(png_path)
+        figure.savefig(png_path, format="png")
+        png_files.append(png_path)
 
-        clip = ImageSequenceClip(sorted(png_files, key=chronological), fps=10)
-        clip.write_videofile(f"{table_directory_path}/cross-attention.mp4", fps=10)
-    finally:
-        rmtree(storage_directory_path)
+    clip = ImageSequenceClip(png_files, fps=10)
+    clip.write_videofile(f"{complex_id}_{head_id}.mp4", fps=15)
