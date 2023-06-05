37bc1f2410b8c400f6cb609f5263be10256b233c
Coos Baakman <cbaakman@radboudumc.nl>
2023-06-05 13:14:43 +0200

    save every 5th batch

--- run.py
+++ run.py
@@ -274,7 +274,7 @@ class Trainer:
                                                    batch_data,
                                                    fine_tune)
 
-            if pdb_output_directory is not None and animated_data is not None and batch_index % self._snap_period == 0:
+            if pdb_output_directory is not None and animated_data is not None and (batch_index + 1) % self._snap_period == 0:
 
                 self._snapshot(f"{epoch_index}.{batch_index + 1}",
                                model,
