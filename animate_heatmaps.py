4ce84ed55cdf7294ab5720c5f980ebc6e0871731
Coos Baakman <cbaakman@radboudumc.nl>
2023-06-09 10:15:56 +0200

    adjust animater to new format

--- animate_heatmaps.py
+++ animate_heatmaps.py
@@ -7,6 +7,7 @@ from tempfile import mkdtemp
 from shutil import rmtree
 from argparse import ArgumentParser
 
+import h5py
 import logging
 import numpy
 import pandas
@@ -18,16 +19,15 @@ _log = logging.getLogger(__name__)
 
 
 arg_parser = ArgumentParser(description="combine tcrspec csv.xz tables into a heatmap animation")
-arg_parser.add_argument("prefix", help="prefix used in the csv.xz filenames")
+arg_parser.add_argument("hdf5_path", help="path to the hdf5 file, containing the data")
+arg_parser.add_argument("data_type", help="type of data to take from hdf5 file: (loop_self_attention/protein_self_attention/cross_attention/other)")
+arg_parser.add_argument("block", type=int, help="number of the block: (0/1/2/...)")
+arg_parser.add_argument("head", type=int, help="number of the head: (0/1/2/...)")
 
 
-def get_epoch_number(path: str) -> float:
+def get_epoch_number(frame_id) -> float:
 
-    # BA-55224-20.160_h0.csv.xz
-    filename = os.path.basename(path)
-
-    word = filename.split('-')[-1]
-    numbers = word.split('.')
+    numbers = frame_id.split('.')
 
     epoch_number = int(numbers[0])
     batch_number = int(numbers[1])
@@ -40,33 +40,33 @@ if __name__ == "__main__":
     args = arg_parser.parse_args()
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-    table_paths = glob(f"{args.prefix}-*.*.csv.xz")
-    table_paths = sorted(table_paths, key=get_epoch_number)
-    if len(table_paths) == 0:
-        raise FileNotFoundError(f"no csv files for prefix {args.prefix}")
+    output_name = os.path.basename(args.hdf5_path).replace(".hdf5","").replace("-animation", "") + f"-{args.data_type}-{args.block}-{args.head}"
 
-    storage_directory_path = mkdtemp()
-    png_files = []
+    with h5py.File(args.hdf5_path, 'r') as hdf5_file:
+        frame_ids = sorted(hdf5_file.keys(), key=get_epoch_number)
 
-    for table_path in table_paths:
-        epoch_number = get_epoch_number(table_path)
+        png_files = []
 
-        data = pandas.read_csv(table_path, compression="xz", sep=',', index_col=0, header=0)
+        for frame_id in frame_ids
 
-        figure = pyplot.figure()
-        plot = figure.add_subplot()
-        vmin=data.min().min()
-        vmax=data.max().max()
-        heatmap = plot.imshow(data, cmap="Greys", aspect="auto", vmin=vmin, vmax=vmax)
-        figure.colorbar(heatmap)
-        pyplot.title(f"{args.prefix}, epoch:{epoch_number:.3f}")
+            epoch_number = get_epoch_number(table_path)
 
-        png_path = table_path.replace(".csv.xz", ".png")
+            data = hdf5_file[f"{frame_id}/{args.data_type}"][args.block, args.head, ...]
 
-        figure.savefig(png_path, format="png")
-        png_files.append(png_path)
+            figure = pyplot.figure()
+            plot = figure.add_subplot()
+            vmin=data.min().min()
+            vmax=data.max().max()
+            heatmap = plot.imshow(data, cmap="Greys", aspect="auto", vmin=vmin, vmax=vmax)
+            figure.colorbar(heatmap)
+            pyplot.title(f"{output_name}, epoch:{epoch_number:.3f}")
 
-        _log.debug(f"created {png_path}")
+            png_path = f"{output_name}.png"
+
+            figure.savefig(png_path, format="png")
+            png_files.append(png_path)
+
+            _log.debug(f"created {png_path}")
 
     clip = ImageSequenceClip(png_files, fps=10)
-    clip.write_videofile(f"{args.prefix}.mp4", fps=15)
+    clip.write_videofile(f"{output_name}.mp4", fps=15)
