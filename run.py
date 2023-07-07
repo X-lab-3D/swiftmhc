47b97be0a3679d39352b1569f29a139dc0cdd6b8
Coos Baakman <baakmanc@int5.local.snellius.surf.nl>
2023-07-07 12:39:22 +0200

    fix the 'dataset already exists' error

--- run.py
+++ run.py
@@ -280,8 +280,11 @@ class Trainer:
                                        compression="lzf")
 
             # save the residue numbering, for later lookup
-            animation_file.create_dataset("protein_residue_numbers", data=data["protein_residue_numbers"][0].cpu())
-            animation_file.create_dataset("loop_residue_numbers", data=data["loop_residue_numbers"][0].cpu())
+            if not "protein_residue_numbers" in animation_file:
+                animation_file.create_dataset("protein_residue_numbers", data=data["protein_residue_numbers"][0].cpu())
+
+            if not "loop_residue_numbers" in animation_file:
+                animation_file.create_dataset("loop_residue_numbers", data=data["loop_residue_numbers"][0].cpu())
 
     def _epoch(self,
                epoch_index: int,
