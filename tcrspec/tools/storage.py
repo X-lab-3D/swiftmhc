aa199db827431cc08e2086709d993488bcf101f7
Coos Baakman <baakmanc@int5.local.snellius.surf.nl>
2023-07-07 13:42:36 +0200

    fixes

--- tcrspec/tools/storage.py
+++ tcrspec/tools/storage.py
@@ -15,9 +15,9 @@ def get_batch_storage_size(batch_data: Dict[str, Any]):
             total_size += sys.getsizeof(value.storage().cpu())
 
         elif isinstance(value, Rigid):
-            total_size += sys.getsizeof(value._trans.storage.cpu())
-            total_size += sys.getsizeof(value._rots._rot_mats.storage.cpu())
-            total_size += sys.getsizeof(value._rots._quats.storage.cpu())
+            total_size += sys.getsizeof(value._trans.storage().cpu())
+            total_size += sys.getsizeof(value._rots._rot_mats.storage().cpu())
+            total_size += sys.getsizeof(value._rots._quats.storage().cpu())
 
         else:
             total_size += sys.getsizeof(value)
