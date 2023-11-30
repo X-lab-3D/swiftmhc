42aa8009042ddc0df100b0e43f879418e8cb276a
Coos Baakman <baakmanc@int5.local.snellius.surf.nl>
2023-11-30 22:02:34 +0100

    store allele_name as bytes value

--- tcrspec/preprocess.py
+++ tcrspec/preprocess.py
@@ -64,11 +64,17 @@ def _write_preprocessed_data(hdf5_path: str, storage_id: str,
 
         protein_group = storage_group.require_group(PREPROCESS_PROTEIN_NAME)
         for field_name, field_data in protein_data.items():
-            protein_group.create_dataset(field_name, data=field_data.cpu(), compression="lzf")
+            if isinstance(field_data, torch.Tensor):
+                field_data = field_data.cpu()
+
+            protein_group.create_dataset(field_name, data=field_data, compression="lzf")
 
         loop_group = storage_group.require_group(PREPROCESS_LOOP_NAME)
         for field_name, field_data in loop_data.items():
-            loop_group.create_dataset(field_name, data=field_data.cpu(), compression="lzf")
+            if isinstance(field_data, torch.Tensor):
+                field_data = field_data.cpu()
+
+            loop_group.create_dataset(field_name, data=field_data, compression="lzf")
 
 
 def _read_targets_by_id(table_path: str) -> List[Tuple[str, Union[float, ComplexClass]]]:
@@ -437,7 +443,7 @@ def preprocess(table_path: str,
             # proximities within protein
             protein_proximities = _create_proximities(protein_residues, protein_residues)
             protein_data["proximities"] = protein_proximities
-            protein_data["allele_name"] = torch.tensor(list(allele.encode("utf_8")))
+            protein_data["allele_name"] = numpy.array(allele.encode("utf_8"))
 
             _write_preprocessed_data(output_path, id_,
                                      protein_data,
