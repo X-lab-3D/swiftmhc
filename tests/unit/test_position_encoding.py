5375cdb19ea93994df55713d4f921519f697c1cb
coosbaakman <coos.baakman@radboudumc.nl>
2024-01-21 21:18:01 +0000

    extra check on the relative position encoding

--- tests/unit/test_position_encoding.py
+++ tests/unit/test_position_encoding.py
@@ -25,5 +25,6 @@ def test_positional_encoding():
         assert torch.all(code[0, 0] == code[5, 5])
         assert torch.all(code[0, 1] == code[5, 6])
         assert torch.all(code[1, 0] == code[6, 5])
+        assert torch.any(code[5, 0] != code[0, 5])
 
         assert torch.any(code[0, 10] != code[2, 5])
