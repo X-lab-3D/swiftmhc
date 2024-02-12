fixes for the NaN gradients in regression mode

swiftmhc/modules/cross_ipa.py
@@ -213,7 +213,7 @@ class CrossInvariantPointAttention(torch.nn.Module):
         pt_att = torch.sum(pt_att, dim=-1) * (-0.5)
 
         # [*, len_dst, len_src]
-        square_mask = dst_mask.unsqueeze(-1) * src_mask.unsqueeze(-2)
+        square_mask = torch.logical_and(dst_mask.unsqueeze(-1), src_mask.unsqueeze(-2))
         square_mask = self.inf * (square_mask.to(dtype=torch.float32) - 1.0)
 
         # [*, H, len_dst, len_src]

swiftmhc/modules/cross_structure_module.py
@@ -416,7 +416,7 @@ class CrossStructureModule(torch.nn.Module):
         positions_CA1 = positions[..., 1:, atom_index_CA, :]
 
         # [*, N_res - 1]
-        mask = res_mask[..., :-1] * res_mask[..., 1:]
+        mask = torch.logical_and(res_mask[..., :-1], res_mask[..., 1:])
         masked_out = torch.logical_not(mask)
 
         # make directional vectors for the 3 bonds
@@ -452,7 +452,8 @@ class CrossStructureModule(torch.nn.Module):
         # [*, N_res - 1]
         omega_sin = (axis * newmann1).sum(dim=-1)
         
-        omega_cos = omega_cos * mask.float() + masked_out.float()
-        omega_sin = omega_sin * mask.float()
+        # masked areas get 180 degrees omega
+        omega_cos = torch.where(mask, omega_cos,-1.0)
+        omega_sin = torch.where(mask, omega_sin, 0.0)
 
         return torch.cat([omega_sin[..., None], omega_cos[..., None]], dim=-1)

swiftmhc/modules/ipa.py
@@ -123,9 +123,8 @@ class DebuggableInvariantPointAttention(torch.nn.Module):
             z[0] = z[0].cpu()
 
         # [*, N_res, N_res]
-        square_mask = mask.unsqueeze(-1) * mask.unsqueeze(-2)
-        general_att_mask = square_mask.clone().unsqueeze(-3)
-        square_mask = (self.inf * (square_mask - 1)).unsqueeze(-3)
+        square_mask = torch.logical_and(mask.unsqueeze(-1), mask.unsqueeze(-2))
+        square_mask = (self.inf * (square_mask.to(dtype=torch.float32) - 1)).unsqueeze(-3)
 
         # [*, H, N_res, N_res]
         if(is_fp16_enabled()):

swiftmhc/modules/position_encoding.py
@@ -170,11 +170,11 @@ class RelativePositionEncoder(torch.nn.Module):
         v = self.linear_q(s).reshape(batch_size, maxlen, self.depth, self.no_heads).transpose(-2, -1).transpose(-3, -2)
 
         # [*, 1, N_res, N_res]
-        square_mask = (masks[:, :, None] * masks[:, None, :]).unsqueeze(-3)
+        square_mask = torch.logical_and(masks[:, :, None], masks[:, None, :]).unsqueeze(-3)
 
         # [*, H, N_res, N_res]
-        a = square_mask * torch.nn.functional.softmax(
-            self.w_L * (torch.matmul(q, k.transpose(-2, -1)) / sqrt(self.depth) + b),
+        a = torch.nn.functional.softmax(
+            self.w_L * (torch.matmul(q, k.transpose(-2, -1)) / sqrt(self.depth) + b) - self.inf * square_mask.float(),
             -1
         )
 

swiftmhc/modules/predictor.py
@@ -39,6 +39,8 @@ class Predictor(torch.nn.Module):
                  config: ml_collections.ConfigDict):
         super(Predictor, self).__init__()
 
+        self.eps = 1e-6
+
         structure_module_config = copy(config.structure_module)
         structure_module_config.c_s = 32
         structure_module_config.c_z = 1
@@ -92,7 +94,7 @@ class Predictor(torch.nn.Module):
         c_transition = 128
         self.affinity_transition = torch.nn.Sequential(
             torch.nn.Linear(structure_module_config.c_s, c_transition),
-            torch.nn.ReLU(),
+            torch.nn.GELU(),
             torch.nn.Linear(c_transition, output_size),
         )
 
@@ -126,6 +128,7 @@ class Predictor(torch.nn.Module):
 
         # [*, peptide_maxlen, c_s]
         peptide_seq = batch["peptide_sequence_onehot"]
+
         batch_size, peptide_maxlen, peptide_depth = peptide_seq.shape
 
         # transform the peptide
@@ -184,8 +187,14 @@ class Predictor(torch.nn.Module):
         # [*, peptide_maxlen, 1]
         mask = batch["peptide_cross_residues_mask"].unsqueeze(-1)
 
+        # [*]
+        length = batch["peptide_cross_residues_mask"].float().sum(dim=-1)
+
+        # [*, peptide_maxlen, output_size]
+        masked_p = torch.where(mask, p, 0.0)
+
         # [*, output_size]
-        ba_output = (p * mask).sum(dim=-2) / mask.sum(dim=-2)
+        ba_output = masked_p.sum(dim=-2) / length.unsqueeze(-1)
 
         # affinity prediction
         if self.model_type == ModelType.REGRESSION: