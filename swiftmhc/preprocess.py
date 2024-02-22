b25fc31ec92083a3d6cbea3a5afb7388b9cd1c53
Coos Baakman <cbaakman@radboudumc.nl>
2024-02-22 14:02:39 +0100

    correct a typo

--- swiftmhc/preprocess.py
+++ swiftmhc/preprocess.py
@@ -40,8 +40,8 @@ from .models.complex import ComplexClass
 _log = logging.getLogger(__name__)
 
 
-PREPROCESS_AFFINNITY_LOWERBOUND_MASK_NAME = "affinity_lowerbound_mask"
-PREPROCESS_AFFINNITY_UPPERBOUND_MASK_NAME = "affinity_upperbound_mask"
+PREPROCESS_AFFINITY_LOWERBOUND_MASK_NAME = "affinity_lowerbound_mask"
+PREPROCESS_AFFINITY_UPPERBOUND_MASK_NAME = "affinity_upperbound_mask"
 PREPROCESS_AFFINITY_NAME = "affinity"
 PREPROCESS_CLASS_NAME = "class"
 PREPROCESS_PROTEIN_NAME = "protein"
@@ -80,8 +80,8 @@ def _write_preprocessed_data(
         if affinity is not None:
             storage_group.create_dataset(PREPROCESS_AFFINITY_NAME, data=affinity)
 
-        storage_group.create_dataset(PREPROCESS_AFFINNITY_LOWERBOUND_MASK_NAME, data=affinity_lt)
-        storage_group.create_dataset(PREPROCESS_AFFINNITY_UPPERBOUND_MASK_NAME, data=affinity_gt)
+        storage_group.create_dataset(PREPROCESS_AFFINITY_LOWERBOUND_MASK_NAME, data=affinity_lt)
+        storage_group.create_dataset(PREPROCESS_AFFINITY_UPPERBOUND_MASK_NAME, data=affinity_gt)
 
         if class_ is not None:
             storage_group.create_dataset(PREPROCESS_CLASS_NAME, data=int(class_))
