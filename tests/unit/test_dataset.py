4bb13432e48ffaa3273a68aa0c9c75bc3da3f3a8
Coos Baakman <coos.baakman@radboudumc.nl>
2024-07-31 23:22:20 +0000

    check that bakcbone frames are read OK

--- tests/unit/test_dataset.py
+++ tests/unit/test_dataset.py
@@ -6,7 +6,9 @@ from swiftmhc.dataset import ProteinLoopDataset
 
 def test_dataset():
 
-    dataset = ProteinLoopDataset("tests/data/data.hdf5", torch.device("cpu"), 16, 200)
+    protein_maxlen = 200
+
+    dataset = ProteinLoopDataset("tests/data/data.hdf5", torch.device("cpu"), 16, protein_maxlen)
     i = dataset.entry_names.index("BA-99998")
 
     peptide_sequence = ''.join([restypes[i] for i in dataset[i]["peptide_sequence_onehot"].nonzero(as_tuple=True)[1]])
@@ -15,3 +17,7 @@ def test_dataset():
     protein_sequence = ''.join([restypes[i] for i in dataset[i]["protein_sequence_onehot"].nonzero(as_tuple=True)[1]])
     assert protein_sequence == "SHSMRYFFTSVSRPGRGEPRFIAVGYVDDTQFVRFDSDAASRRMEPRAPWIEQEGPEYWDGETRKVKAHSQTHRVDLGTLRGYYNQSEAGSHTLQRMYGCDVGSDWRFLRGYHQYAYDGKDYIALKEDLRSWTAADMAAQTTKHKWEAAHVAEQWRAYLEGTCVEWLRRYLENGKETLQR"
 
+    rigid_tensor = dataset[i]["protein_backbone_rigid_tensor"]
+    assert rigid_tensor.shape == (protein_maxlen, 4, 4)
+
+    assert (rigid_tensor[0] != rigid_tensor[1]).any()
