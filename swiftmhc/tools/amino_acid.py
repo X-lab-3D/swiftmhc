b07b12453083c8b806b632fc96ce9d1b94e41e00
Coos Baakman <cbaakman@radboudumc.nl>
2024-08-28 12:29:11 +0200

    add a function for stretching the peptide sequence

--- swiftmhc/tools/amino_acid.py
+++ swiftmhc/tools/amino_acid.py
@@ -1,4 +1,5 @@
-from typing import List, Union
+from typing import List, Union, Tuple
+from math import floor
 
 import torch
 
@@ -43,3 +44,36 @@ def one_hot_decode_sequence(encoded_sequence: torch.Tensor) -> List[Union[AminoA
 
     return amino_acids
 
+
+def stretch_sequence(encoded_sequence: torch.Tensor, new_length: int) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Encodes a sequence as a masked sequence with given length.
+    XXXXXXXXX becomes XXXX----X---XXXX
+
+    Args:
+        encoded_sequence [L, D]
+        new_length(N)
+    Returns:
+        stretched sequence [N, D]
+        mask [N]
+    """
+
+    if encoded_sequence.shape[0] < 8:
+        raise ValueError(f"sequence of length {encoded_sequence.shape[0]} is too short, at least 8 required")
+
+    center_i = int(floor(new_length / 2))
+    center_length = encoded_sequence.shape[0] - 8
+    center_nlength = int(floor(center_length / 2))
+    center_clength = center_length - center_nlength
+
+    stretched_sequence = encoded_sequence.new_zeros([new_length] + list(encoded_sequence.shape[1:]))
+    stretched_sequence[:4] = encoded_sequence[:4]
+    stretched_sequence[center_i - center_nlength: center_i + center_clength] = encoded_sequence[4: -4]
+    stretched_sequence[-4:] = encoded_sequence[-4:]
+
+    mask = encoded_sequence.new_zeros([new_length], dtype=torch.bool)
+    mask[:4] = True
+    mask[center_i - center_nlength: center_i + center_clength] = True
+    mask[-4:] = True
+
+    return stretched_sequence, mask
