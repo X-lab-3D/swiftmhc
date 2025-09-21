fix normalization operation order: first LayerNorm then Dropout for
The change will be aligned with the formula in the manuscript.

swiftmhc/modules/peptide_attention.py
@@ -51,10 +51,7 @@ class PeptideSelfAttention(torch.nn.Module):
         )
 
         # to be used after multi-headed attention
-        self.norm1 = torch.nn.Sequential(
-            torch.nn.Dropout(self.dropout_rate),
-            LayerNorm(self.c_s),
-        )
+        self.norm1 = torch.nn.Sequential(LayerNorm(self.c_s), torch.nn.Dropout(self.dropout_rate))
 
         # to be used after multi-headed attention norm
         self.feed_forward = torch.nn.Sequential(
@@ -64,10 +61,7 @@ class PeptideSelfAttention(torch.nn.Module):
         )
 
         # to be used after feed-forward
-        self.norm2 = torch.nn.Sequential(
-            torch.nn.Dropout(self.dropout_rate),
-            LayerNorm(self.c_s),
-        )
+        self.norm2 = torch.nn.Sequential(LayerNorm(self.c_s), torch.nn.Dropout(self.dropout_rate))
 
     def forward(self, s: torch.Tensor, mask: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         """Encodes a sequence, by means of self attention and feed forward MLP