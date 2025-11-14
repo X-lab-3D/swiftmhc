e549c59e3e8dc8b0260e49efc13b6e0ed974a840
cbaakman <c.baakman@umcn.nl>
2025-11-14 13:29:22 +0100

    Update rmsd-calpha-mhc-9mer-peptides.bash
    add a comment about profit

--- scripts/evaluation/rmsd-calpha-mhc-9mer-peptides.bash
+++ scripts/evaluation/rmsd-calpha-mhc-9mer-peptides.bash
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# this script requires profit to work.
+
 if [ $# -ne 2 ] ; then
-    echo "Usage: $0 reference_directory mobile_directory"
+    echo "Usage: $0 re:wqference_directory mobile_directory"
     exit 0
 fi
 
